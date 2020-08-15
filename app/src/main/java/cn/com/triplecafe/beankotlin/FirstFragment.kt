32c722e3645869b949e8ae889eddf6915aa502ca
李秋 <arthur@industryillusion.com>
2020-08-15 17:02:24 +0800

    UI改造：设置两个 Button

--- app/src/main/java/cn/com/triplecafe/beankotlin/FirstFragment.kt
+++ app/src/main/java/cn/com/triplecafe/beankotlin/FirstFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -24,8 +24,13 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        view.findViewById<Button>(R.id.button_download_first).setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            Toast.makeText(this.context, "点击了 button 1", Toast.LENGTH_SHORT).show();
+        }
+
+        view.findViewById<Button>(R.id.button_download_second).setOnClickListener {
+            Toast.makeText(this.context, "点击了 button 2", Toast.LENGTH_SHORT).show()
         }
     }
 }
